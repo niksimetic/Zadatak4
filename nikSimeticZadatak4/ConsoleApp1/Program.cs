8fe6e5679cb17223c4bad3e9e99e3e26292e5435
niksimetic <55134764+niksimetic@users.noreply.github.com>
2020-11-17 10:50:09 +0100

    Add files via upload

---
+++ nikSimeticZadatak4/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi cijenu bez PDV-a: ");
+            double bezPDV = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Unesite istu cijenu sa PDV-om");
+
+            double saPDV = bezPDV * 1.25; 
+
+            Console.WriteLine("Cijena sa PDV-om je :"+saPDV);
+            Console.ReadKey();
+        }
+    }
+}
